4f6e16e018c5a8bac2134d8b4d1db21e69d2b69f
Nynne <nynnejc@gmail.com>
2015-09-20 15:32:26 +0200

    added img

--- js/script.js
+++ js/script.js
@@ -5,4 +5,7 @@ $(document).ready(function() {
           verticalCentered: false,
           
       });
-      });
+    $(".galleryimage").click(function( event ) {
+        $(".nextimagediv").show();
+    });
+});
