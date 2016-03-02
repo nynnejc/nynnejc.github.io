2e43518b799b70c2229cac4c1ece2c13b548808b
Nynne <nynnejc@gmail.com>
2016-03-02 18:02:08 +0100

    img add

--- js/script.js
+++ js/script.js
@@ -8,4 +8,25 @@ $(document).ready(function() {
     $(".galleryimage").click(function( event ) {
         $(".nextimagediv").first().show().removeClass("nextimagediv");
     });
+
+
+
+    var imgHTML = '<img class="img galleryimage" src="img/project-img">'
+
+    var add_new_img = function( event ) {
+    
+      var top = Math.round(Math.random()* 800) + "px"
+      var left = Math.round(Math.random()* 1000) + "px"
+
+        var img = $(imgHTML).css({
+          width: width,
+          left: left,
+          top: top
+        });
+
+        $("#projects").append(img);
+    };
+
+    $("#projects").on("click", ".galleryimage", add_new_img);
+
 });
