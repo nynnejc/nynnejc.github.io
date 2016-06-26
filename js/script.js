25a5d082152ea7512cd0d480dff0bf6dd9418274
Nynne <nynnejc@gmail.com>
2016-06-26 13:17:29 +0200

    removed pagepiling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-        $('#pagepiling').pagepiling({
-          anchors: ['home', 'projects', 'contact'],
-          navigation: false,
-          verticalCentered: false,
-          
-      });
+      
     $(".galleryimage").click(function( event ) {
         $(".nextimagediv").first().show().removeClass("nextimagediv");
     });
